fde6fa48f5c9aa61c5a899114469afff145f5bcc
JM Fantin <jmfantin@outlook.com>
2022-11-03 04:20:49 -0300

    carousel dynamics ready

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,10 +1,41 @@
 import lits from "../../lib/lits";
-
+import shift from "classnames";
+import { useState } from "react";
 export function Home() {
+  const [litIndex, setLitIndex] = useState(0);
+  //save lit list index with useState
   return (
-    <div className="max-w-[1344px] flex flex-col justify-center items-center bg-nlw-gradient h-[100vh] w-[100%]">
-      {/*GradientBG*/}
-      <h1>{lits[0].author}</h1>
-    </div>
+    <>
+      {/*GradientBG //use custom styling with classnames*/}
+      <div className="max-w-[1344px] flex flex-col justify-center items-center bg-nlw-gradient h-[100vh] w-[100%]">
+        {/*LitCard*/}
+        <h1>{lits[litIndex].author}</h1>
+        <h1>{litIndex}</h1>
+        {/*PreviousCard*/}
+        <button
+          onClick={() => {
+            if (litIndex > 0) {
+              setLitIndex(litIndex - 1);
+            } else {
+              setLitIndex(lits.length - 1);
+            }
+          }}
+        >
+          Prev
+        </button>
+        {/*NextCard*/}
+        <button
+          onClick={() => {
+            if (litIndex < lits.length - 1) {
+              setLitIndex(litIndex + 1);
+            } else {
+              setLitIndex(0);
+            }
+          }}
+        >
+          Next
+        </button>
+      </div>
+    </>
   );
 }
--- src/lib/lits.ts
+++ src/lib/lits.ts
@@ -1,9 +1,23 @@
 export default [
   {
-    title: 'O dia em que o H fugiu do alfabeto.',
-    subtitle: 'Fugiu por quê? Para onde foi? Investigue com a turma.',
-    author: 'Helplit',
-    stage: 'Aprendizagem Lúdica',
-    picturePath: '/litImages/h.png',
+    title: "O dia em que o H fugiu do alfabeto.",
+    subtitle: "Fugiu por quê? Para onde foi? Investigue com a turma.",
+    author: "JM",
+    stage: "Aprendizagem Lúdica",
+    picturePath: "/litImages/h.png",
+  },
+  {
+    title: "O dia em que o H fugiu do alfabeto.",
+    subtitle: "Fugiu por quê? Para onde foi? Investigue com a turma.",
+    author: "Ro",
+    stage: "Aprendizagem Lúdica",
+    picturePath: "/litImages/h.png",
+  },
+  {
+    title: "O dia em que o H fugiu do alfabeto.",
+    subtitle: "Fugiu por quê? Para onde foi? Investigue com a turma.",
+    author: "Tamara",
+    stage: "Aprendizagem Lúdica",
+    picturePath: "/litImages/h.png",
   },
 ].sort(() => Math.random() - 0.5);
