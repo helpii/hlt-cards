2e5091d69d264cdca051ed4fd75c4f3190e8103c
JM Fantin <jmfantin@outlook.com>
2022-11-14 10:36:31 -0300

    deck position correction

--- src/components/LitDeck/index.tsx
+++ src/components/LitDeck/index.tsx
@@ -38,66 +38,66 @@ export function LitDeck() {
   }, [stage]);
 
   return (
-    <a href={litList[drawed].classPlanUrl} target="blank">
+    <a
+      href={litList[drawed].classPlanUrl}
+      target="blank"
+      className={shift(
+        "rounded-b-[30px] hover:border-HELP_weak border-b-8 hover:bg-[#f7f7f7] border-[#b9b9b9] hover:cursor-pointer rounded-xl max-w-[90%] bg-white w-[401px] p-2 h-[500px] max-h-[80vh] mb-4 flex flex-col justify-between items-center transition-colors",
+        {
+          "hover:border-SINV_weak": stage === "SINV",
+          "hover:border-ALUD_weak": stage === "ALUD",
+          "hover:border-PCOM_weak": stage === "PCOM",
+        }
+      )}
+    >
       <div
         className={shift(
-          "rounded-b-[30px] group hover:border-HELP_weak border-b-8 hover:bg-[#f7f7f7] border-[#b9b9b9] hover:cursor-pointer rounded-xl max-w-[90%] bg-white w-[401px] p-2 h-[500px] max-h-[80vh] mb-4 flex flex-col justify-between items-center transition-colors",
+          "group-hover:bg-[#f7f7f7] absolute self-start rounded-br-lg bg-white pb-[6px] pr-[6px] text-HELP_stronk font-semibold flex flex-row gap-1 items-center transition-colors",
           {
-            "hover:border-SINV_weak": stage === "SINV",
-            "hover:border-ALUD_weak": stage === "ALUD",
-            "hover:border-PCOM_weak": stage === "PCOM",
+            "text-ALUD_stronk": stage === "ALUD",
+            "text-SINV_stronk": stage === "SINV",
+            "text-PCOM_stronk": stage === "PCOM",
           }
         )}
       >
+        <div className="bg-helplit_sm w-8 h-8 bg-cover rounded-full"></div>
+        <p>@helplit</p>
+      </div>
+      <img
+        className="rounded-lg"
+        src={litList[drawed].picturePath}
+        width="389"
+        height="257"
+      />
+      {/*Lower Card Section*/}
+      <div className="flex flex-col items-center p-2 text-center">
+        <h1
+          className={shift("text-lg mb-1 border-b-2 border-b-HELP_weak", {
+            "border-b-SINV_weak": stage === "SINV",
+            "border-b-ALUD_weak": stage === "ALUD",
+            "border-b-PCOM_weak": stage === "PCOM",
+          })}
+        >
+          {litList[drawed].title}
+        </h1>
+        <p>{litList[drawed].subtitle}</p>
+      </div>
+      {/*Card Footer Info*/}
+      <div className=" w-[100%] mt-2 flex flex-row justify-center items-center">
         <div
           className={shift(
-            "group-hover:bg-[#f7f7f7] absolute self-start rounded-br-lg bg-white pb-[6px] pr-[6px] text-HELP_stronk font-semibold flex flex-row gap-1 items-center transition-colors",
+            "py-1 pl-2 pr-3 rounded-2xl text-clip overflow-hidden font-semibold text-sm text-white flex flex-row items-center gap-1",
             {
-              "text-ALUD_stronk": stage === "ALUD",
-              "text-SINV_stronk": stage === "SINV",
-              "text-PCOM_stronk": stage === "PCOM",
+              "bg-ALUD_stronk": litList[drawed].stage === "ALUD",
+              "bg-SINV_stronk": litList[drawed].stage === "SINV",
+              "bg-PCOM_stronk": litList[drawed].stage === "PCOM",
             }
           )}
         >
-          <div className="bg-helplit_sm w-8 h-8 bg-cover rounded-full"></div>
-          <p>@helplit</p>
-        </div>
-        <img
-          className="rounded-lg"
-          src={litList[drawed].picturePath}
-          width="389"
-          height="257"
-        />
-        {/*Lower Card Section*/}
-        <div className="flex flex-col items-center p-2 text-center">
-          <h1
-            className={shift("text-lg mb-1 border-b-2 border-b-HELP_weak", {
-              "border-b-SINV_weak": stage === "SINV",
-              "border-b-ALUD_weak": stage === "ALUD",
-              "border-b-PCOM_weak": stage === "PCOM",
-            })}
-          >
-            {litList[drawed].title}
-          </h1>
-          <p>{litList[drawed].subtitle}</p>
-        </div>
-        {/*Card Footer Info*/}
-        <div className=" w-[100%] mt-2 flex flex-row justify-center items-center">
-          <div
-            className={shift(
-              "py-1 pl-2 pr-3 rounded-2xl text-clip overflow-hidden font-semibold text-sm text-white flex flex-row items-center gap-1",
-              {
-                "bg-ALUD_stronk": litList[drawed].stage === "ALUD",
-                "bg-SINV_stronk": litList[drawed].stage === "SINV",
-                "bg-PCOM_stronk": litList[drawed].stage === "PCOM",
-              }
-            )}
-          >
-            <>
-              {getStageIcon(litList[drawed].stage)}
-              <p>{getStageName(litList[drawed].stage)}</p>
-            </>
-          </div>
+          <>
+            {getStageIcon(litList[drawed].stage)}
+            <p>{getStageName(litList[drawed].stage)}</p>
+          </>
         </div>
       </div>
     </a>
